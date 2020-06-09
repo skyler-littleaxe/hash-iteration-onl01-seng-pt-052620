56dc3da9b50c6b172fbd733c4d21bd4a737008eb
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-09 01:57:05 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,8 +15,8 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age| 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-      unless age > 13
-    end
+     if age > 12
+     else puts 
      
   end
 end
